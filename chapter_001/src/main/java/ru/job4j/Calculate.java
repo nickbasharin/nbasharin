91120c9cfbb06a60c8ee14915ce53edc6304a498
nickbasharin <bni1284@gmail.com>
2018-01-02 22:39:26 +0300

    1.5 Checkstyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,13 +3,22 @@ package ru.job4j;
 /**
 *Класс вывода строки в консоль
 */
-public class Calculate{
+public class Calculate {
 	/**
 	* Конструктор вывода строки в консоль
 	* @param arg - arg
 	*/
-	public static void main(String[] arg){
+	public static void main(String[] arg) {
 		System.out.println("Hello world.");
 	}
+	
+	/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
 	
 }
