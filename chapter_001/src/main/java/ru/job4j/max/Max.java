8bf3ad502ba276ac7b5a15d6a44d683c966db8f6
nickbasharin <bni1284@gmail.com>
2018-01-04 16:05:54 +0300

    3.2. Максимум из двух чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,15 @@
+package ru.job4j.max;
+
+/**
+ *
+ * @author Nikolai Basharin(nick712@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+
+    public int max(int first, int second) {
+        return (first > second) ? first : second;
+    }
+
+}
